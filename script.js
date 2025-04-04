d16fa4c9f8085eeefa324d0c3e240f72af2ec1c9
Matheus2614 <matheuss.wincler.senai@gmail.com>
2025-04-04 08:05:57 -0300

    Primerio commit

---
+++ script.js
@@ -0,0 +1,50 @@
+
+const baseUrl = "http://127.0.0.1:5000/";
+const aleatorio = "charadas";
+
+const pergunta = document.getElementById("charada")
+const resposta = document.getElementById("resposta-container")
+
+let charadajson;
+
+async function getCharada() {
+    resposta.innerHTML = ''
+
+    try {
+        
+        const charada = await fetch(baseUrl + aleatorio)
+        charadajson = await charada.json();
+        console.log(charadajson.charada);
+
+        pergunta.innerHTML = `${charadajson.charada}`
+
+    }
+
+    catch (error) {
+        console.log("Erro ao chamar a API:" + error)
+    }
+}
+
+function conferir() {
+    try {
+
+        const respostaUsuario = document.getElementById('resposta').value;
+        const respostaCerta = charadajson.resposta
+
+        if (respostaUsuario.toLowerCase() === respostaCerta.toLowerCase()) {
+            resposta.innerHTML = `Parabéns! A reposta era "${respostaUsuario}"`
+        }
+
+        else {
+            resposta.innerHTML = `Não foi dessa vez. A resposta era "${respostaCerta}"`
+        }
+
+
+    }
+
+    catch (error) {
+        console.log("Erro ao chamar a API:" + error)
+    }
+}
+
+getCharada()
